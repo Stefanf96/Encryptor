d363fa34c2bc301e67990ebeb60c526a649d0482
Stefan Filippo <54752442+Stefanf96@users.noreply.github.com>
2021-08-13 17:41:15 +0200

    Encryption + Open Automatically
    Encryption done + System to open txt file after creation

--- Encryptor/Form1.cs
+++ Encryptor/Form1.cs
@@ -9,18 +9,20 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Security.Cryptography;
+using System.Diagnostics;
 
 
 namespace Encryptor
 {
     public partial class Form1 : Form
     {
+        StreamWriter file;
         private string documentName;
         private string documentContent;
         private bool IsEncrypted;
-        private string key = "sblw-3hn8-sqoy19";
-   /*     private string SaveLocation = "C:\\Users\\Stefan\\Desktop";*/
-
+        private string key = @"sblw-3hn8-sqoy19";
+        private string SaveLocation = @"D:\Documenten\Test Files\";
+        private string dataType = '.txt';-
         public Form1()
         {
             InitializeComponent();
@@ -29,9 +31,10 @@ namespace Encryptor
         private void CreateDocument_Click(object sender, EventArgs e)
         {
             documentName = DocumentNameBox.Text;
-            Encrypted.Checked = IsEncrypted;
+            IsEncrypted = Encrypted.Checked;
             if (IsEncrypted)
             {
+                MessageBox.Show(IsEncrypted.ToString());
                 documentContent = Encrypt(ContentDocument.Text, key);
             }
             else
@@ -39,10 +42,34 @@ namespace Encryptor
                 documentContent = ContentDocument.Text;
             }
             
-            TextCreator(documentName, documentContent);
+             TextCreator(documentName, documentContent);
         }
-        public static async Task TextCreator(string name,  string content) {
-            await File.WriteAllTextAsync(name + ".txt", content);
+        private void TextCreator(string name, string content) {
+            try
+            {
+                string fullPath = SaveLocation+name+dataType;
+                using (StreamWriter writer = new StreamWriter(fullPath))
+                {
+                    // Add some text to file    
+                    writer.WriteLine(content);
+                }
+                if (File.Exists(fullPath))
+                { 
+                    var process = Process.Start("notepad.exe", fullPath);
+                    process.WaitForInputIdle();
+                    var handle = process.MainWindowHandle;
+
+                    // if the window is still in the foreground
+                    SendKeys.SendWait("^(s)"); // Ctrl+S
+                  
+                }
+
+            }
+            catch(Exception e) {
+                MessageBox.Show("Exception: " + e.Message);
+            }
+            
+
         }
         public static string Encrypt(string input, string key)
         {
