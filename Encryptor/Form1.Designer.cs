5a4db6786463e749872c593dabb402087427b321
Stefan Filippo <54752442+Stefanf96@users.noreply.github.com>
2021-08-13 16:15:59 +0200

    Initial commit

---
+++ Encryptor/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Encryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CreateDocument = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TextBox = new System.Windows.Forms.Label();
+            this.DocumentName = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CreateDocument
+            // 
+            this.CreateDocument.Location = new System.Drawing.Point(148, 356);
+            this.CreateDocument.Name = "CreateDocument";
+            this.CreateDocument.Size = new System.Drawing.Size(153, 62);
+            this.CreateDocument.TabIndex = 0;
+            this.CreateDocument.Text = "Create Document";
+            this.CreateDocument.UseVisualStyleBackColor = true;
+            this.CreateDocument.Click += new System.EventHandler(this.CreateDocument_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(148, 328);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 19);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(148, 176);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(473, 137);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(473, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // TextBox
+            // 
+            this.TextBox.AutoSize = true;
+            this.TextBox.Location = new System.Drawing.Point(148, 155);
+            this.TextBox.Name = "TextBox";
+            this.TextBox.Size = new System.Drawing.Size(28, 15);
+            this.TextBox.TabIndex = 4;
+            this.TextBox.Text = "Text";
+            // 
+            // DocumentName
+            // 
+            this.DocumentName.AutoSize = true;
+            this.DocumentName.Location = new System.Drawing.Point(148, 40);
+            this.DocumentName.Name = "DocumentName";
+            this.DocumentName.Size = new System.Drawing.Size(98, 15);
+            this.DocumentName.TabIndex = 5;
+            this.DocumentName.Text = "Document Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(148, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Document Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(148, 113);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(473, 23);
+            this.textBox3.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.DocumentName);
+            this.Controls.Add(this.TextBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.CreateDocument);
+            this.Name = "Form1";
+            this.Text = "Encryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CreateDocument;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label TextBox;
+        private System.Windows.Forms.Label DocumentName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
